8c38768c831e6dd3084e186908ea4a8ef18f29f1
jasminexo <56755376+jasminexo@users.noreply.github.com>
2021-11-03 22:26:15 +1100

    jas edit

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //testing - mai
         //testing - joc
+        //testing - jas
     }
 }
