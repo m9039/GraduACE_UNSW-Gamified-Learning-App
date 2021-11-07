9e2d5bef3ed8a5f62978d31506bfdbb872a94977
m9039 <61225254+m9039@users.noreply.github.com>
2021-11-07 18:15:16 +1100

    simple login/register xml

---
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/RegisterActivity.java
@@ -0,0 +1,21 @@
+package au.edu.unsw.infs3634.unswgamifiedlearningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void goToLogin(View view){
+        Intent intent = new Intent (this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
