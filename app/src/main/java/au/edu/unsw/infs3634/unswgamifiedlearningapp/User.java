b8c110973953e34497cfb1c4e01fd6dfefce0cfb
m9039 <61225254+m9039@users.noreply.github.com>
2021-11-18 09:39:41 +1100

    firebase login

---
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/User.java
@@ -0,0 +1,16 @@
+package au.edu.unsw.infs3634.unswgamifiedlearningapp;
+
+public class User {
+
+    public String fullname, username, email;
+
+    public User(){
+
+    }
+
+    public User(String fullname, String username, String email) {
+        this.fullname = fullname;
+        this.username = username;
+        this.email = email;
+    }
+}
